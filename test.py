03b9b2463d88c0624ac72ad4817ea35b139aac2c
kndambuki <48718299+kndambuki@users.noreply.github.com>
2019-07-02 14:40:32 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,14 +1,14 @@
 import random 
 
 print("Changing local repo")
-#~ filename = "test.txt"
-#~ random_num = random.randrange(0, 50)
-#~ while (random_num != 16):
-	#~ try:
-		#~ with open(filename, 'a') as f:
-			#~ f.write("ON THIS MONDAY, Is this your number?: "+str(random_num) + "\n")
-			#~ print("ON THIS MONDAY, I am random number..", random_num)
-	#~ except FileNotFoundError:
-		#~ print("Noma sana")
+filename = "test.txt"
+random_num = random.randrange(0,20)
+while (random_num != 16):
+	try:
+		with open(filename, 'a') as f:
+			f.write("ON THIS Tuesdat, Is this your number?: "+str(random_num) + "\n")
+			print("ON THIS TUESDATY, I am random number..", random_num)
+	except FileNotFoundError:
+		print("Noma sana")
 		
-	#~ random_num = random.randrange(0, 50)
+	random_num = random.randrange(0, 20)
